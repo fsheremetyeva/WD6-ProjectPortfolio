70d9af86bb966b11e5ef81050c65629afc64c16b
Jessica Karpovich <karpovichjm@gmail.com>
2018-10-27 18:26:02 -0500

    Changed view to output db product data

--- Team Development/shopping-cart/resources/views/shop/index.blade.php
+++ Team Development/shopping-cart/resources/views/shop/index.blade.php
@@ -10,12 +10,12 @@
             @foreach($productChunk as $product)
               <div class="col-sm-6 col-md-4">
                 <div class="card" style="width: 18rem;">
-                  <img class="card-img-top img-responsive" src="https://static.tvtropes.org/pmwiki/pub/images/harrypotterandthegobletoffire.jpg" alt="Card image cap" >
+                  <img class="card-img-top img-responsive" src="{{ $product->imagePath }}" alt="Card image cap" >
                   <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 class="card-title">{{ $product->title }}</h5>
+                    <p class="card-text">{{ $product->description }}</p>
                     <div class="clearfix">
-                      <div class="float-left price">$56.00</div>
+                      <div class="float-left price">${{ $product->price }}</div>
                        <a href="#" class="btn btn-success float-right">Add to Cart</a>
                     </div>
                   </div>
