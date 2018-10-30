6767161a2a3263047ae80e467c1aa37aa90e7d73
Jessica Karpovich <karpovichjm@gmail.com>
2018-10-30 00:02:37 -0500

    Setup route and view to test ATC

--- Team Development/shopping-cart/app/Http/Controllers/ProductController.php
+++ Team Development/shopping-cart/app/Http/Controllers/ProductController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Cart;
 use Illuminate\Http\Request;
+
+use Session;
 
 class ProductController extends Controller
 {
@@ -15,6 +18,12 @@ class ProductController extends Controller
     
     public function getAddToCart(Request $request, $id) {
         $product = Product::find($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
         
+        $request->session()->put('cart', $cart);
+//        dd($request->session()->get('cart'));
+        return redirect()->route('product.index');
     }
 }
--- Team Development/shopping-cart/resources/views/shop/index.blade.php
+++ Team Development/shopping-cart/resources/views/shop/index.blade.php
@@ -16,7 +16,7 @@
                     <p class="card-text">{{ $product->description }}</p>
                     <div class="clearfix">
                       <div class="float-left price">${{ $product->price }}</div>
-                       <a href="#" class="btn btn-success float-right">Add to Cart</a>
+                       <a href="{{ route('product.addToCart', ['id' => $product->id]) }}" class="btn btn-success float-right">Add to Cart</a>
                     </div>
                   </div>
                 </div>
