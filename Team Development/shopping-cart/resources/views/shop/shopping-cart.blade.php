772c7cfb7f143c5a5f2c67220552e86b67e415f6
Jessica Karpovich <karpovichjm@gmail.com>
2018-10-31 21:23:40 -0500

    Adjusted styling for Bootstrap4

--- Team Development/shopping-cart/resources/views/shop/shopping-cart.blade.php
+++ Team Development/shopping-cart/resources/views/shop/shopping-cart.blade.php
@@ -10,25 +10,25 @@
             <div class="col-sm-6 col-sm-offset-3">
                 <ul class="list-group">
                     @foreach($products as $product)
-                        <li class="list-group-item">
-                            <span class="badge badge-secondary">{{ $product['qty'] }}</span>
+                        <li class="list-group-item d-flex align-items-center">
                             <strong>{{ $product['item']['title'] }}</strong>
-                            <span class="label label-success">{{ $product['price'] }}</span>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">Action 
+                            <span class="badge badge-success mx-2">{{ $product['price'] }}</span>
+                            <div class="dropdown">
+                                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" id="dropdownBtn">Action 
                                     <span class="caret"></span>
                                 </button>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Reduce by 1</a></li>
-                                    <li><a href="#">Reduce All</a></li>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownBtn">
+                                    <li class="dropdown-item"><a href="#">Reduce by 1</a></li>
+                                    <li class="dropdown-item"><a href="#">Reduce All</a></li>
                                 </ul>
                             </div>
+                            <span class="badge badge-secondary ml-auto">{{ $product['qty'] }}</span>
                         </li>
                     @endforeach
                 </ul>
             </div>
         </div>
-        <div class="row">
+        <div class="row my-2">
             <div class="col-sm-6 col-sm-offset-3">
                 <strong>Total: {{ $totalPrice }}</strong>
             </div>
