0399e81ff910bcddd635d218510b2471962012b4
Jessica Karpovich <karpovichjm@gmail.com>
2018-11-04 15:22:35 -0600

    Started Profile view setup to display orders

--- Team Development/shopping-cart/resources/views/user/profile.blade.php
+++ Team Development/shopping-cart/resources/views/user/profile.blade.php
@@ -2,8 +2,22 @@
 @extends('layouts.master')
 @section('content')
   <div class="row">
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-8 col-md-offset-2">
       <h1>user Profile</h1>
+      <hr>    
+        
+      <div class="card text-center">
+        <div class="card-body">
+          <h5 class="card-title">My Orders</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional  content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Cras justo odio</li>
+        </ul>
+        <div class="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
     </div>
   </div>
 @endsection
