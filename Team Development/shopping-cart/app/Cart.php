9aa4cfb88a64f5efb94f8280259c8851205565fa
Jessica Karpovich <karpovichjm@gmail.com>
2018-11-04 16:02:47 -0600

    Changed removeOne to unset item if qty is 0

--- Team Development/shopping-cart/app/Cart.php
+++ Team Development/shopping-cart/app/Cart.php
@@ -41,5 +41,18 @@ class Cart
         $this->items[$id]['price'] -= $this->items[$id]['item']['price'];
         $this->totalQty--;
         $this->totalPrice -= $this->items[$id]['item']['price'];
+        
+        if ($this->items[$id]['qty'] <= 0) {
+            // destroy item from cart
+            unset($this->items[$id]);
+        }
+    }
+    
+    public function removeItem($id) {
+        $this->totalQty -= $this->items[$id]['qty'];
+        $this->totalPrice -= $this->items[$id]['price'];
+
+        // destroy item from cart
+        unset($this->items[$id]);
     }
 }
